2bfba69e8fbc44f7336777069c4f71132b8fbb08
Fionapple7 <doggy.dog.the.3rd@gmail.com>
2025-07-15 20:02:19 +0900

    ハンバーガーメニューの修正
    クリックで開き、メニューとSNS表示、リンク先追加

--- js/main.js
+++ js/main.js
@@ -9,5 +9,45 @@ gsap.timeline()
     "-=0.5"
   );
 
+const hamIcon = document.querySelector(".ham-icon");
+const hamPanel = document.querySelector(".ham-menu__panel");
+const hamLinks = document.querySelectorAll(".ham-menu__panel a");
+let menuOpen = false;
 
-  
+// 開くアニメーション
+const openMenu = () => {
+  hamPanel.style.display = "flex"; // 表示切り替え（GSAPでアニメーション）
+
+  gsap.fromTo(hamPanel,
+    { opacity: 0, y: -30 },
+    { opacity: 1, y: 0, duration: 0.6, ease: "power2.out" }
+  );
+
+  menuOpen = true;
+};
+
+// 閉じるアニメーション
+const closeMenu = () => {
+  gsap.to(hamPanel, {
+    opacity: 0,
+    y: -30,
+    duration: 0.4,
+    ease: "power2.in",
+    onComplete: () => {
+      hamPanel.style.display = "none"; // 表示解除
+      menuOpen = false;
+    }
+  });
+};
+
+// ハンバーガーアイコンをクリック
+hamIcon.addEventListener("click", () => {
+  menuOpen ? closeMenu() : openMenu();
+});
+
+// メニューリンクをクリックしたら閉じる
+hamLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    if (menuOpen) closeMenu();
+  });
+});
